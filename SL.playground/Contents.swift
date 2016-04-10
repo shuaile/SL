2dbb484ebd33b58acb1030c29ec311b3d22b61d8
shuaile <18736380266@163.com>
2016-04-10 21:21:07 +0800

    SL

--- SL.playground/Contents.swift
+++ SL.playground/Contents.swift
@@ -5,14 +5,53 @@ import UIKit
 var str = "Hello, SL"
 
 
-if true {
 
-}
+
 
 /***
     屏蔽掉节省内存资源
  */
 #if false
 
+if true {
+    
+    let minValue = UInt8.min
+    let maxValue = UInt8.max
+}
+
+    
+    
+    //printing
+if true {
+    
+    print(str)
+    
+    print("welcome \(str)")
+}
+    
+    //type annotations
+if true {
+    
+    var string: String
+    
+    string = "hellp sl"
+    
+    string = "hello sl++"
+    
+    
+    var red , green, blue: Double
+}
+    
+    
+    //declaring constants and variables
+if true {
+    
+    let maximunNumberOfLoginAttempts = 10
+    
+    var currentLoginAttempt = 0
+    
+    
+    var x = 0.0, y = 0.0, z = 0.0
+}
 
 #endif
